0b40d6688c61c74ed8bdb0f98b9048ef2a1da4a3
Florence Kavita <florencekavita@MacBook-Air-de-Florence.local>
2016-05-09 10:38:13 +0200

    zer

---
+++ Nike.js
@@ -0,0 +1,7 @@
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 100,
+        freeMode: true
+    });
